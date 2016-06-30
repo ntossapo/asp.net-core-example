63ac9c7dd2699e6400495f5fb1024ec135808089
Thotsapon Nuanchuay <ntossapo@gmail.com>
2016-06-30 10:19:08 +0700

    init

---
+++ src/asp_net_core_example/Controllers/myController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace asp_net_core_example.Controllers
+{
+    [Route("hello")]
+    public class myController : Controller
+    {
+        [HttpGet("{value}")]
+        public string Fibonacci(int value)
+        {
+            return "Hello this is .NET core " + value;
+        }
+
+        public static bool isFibonacci(int target)
+        {
+            bool result = false;
+            int value1 = 1;
+            int value2 = value1;
+        
+            while(value2 <= target)
+            {
+                int temp = value2;
+                value2 += value1;
+                value1 = temp;
+            }
+
+            if(value1 == target)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/asp_net_core_example/Tests/FibonacciTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using asp_net_core_example.Controllers;
+
+namespace asp_net_core_example.Tests
+{
+    public class FibonacciTest
+    {
+        [Fact]
+        public void itShouldBeTrue()
+        {
+            Assert.Equal(true, true);           
+        }
+
+        [Fact]
+        public void itShouldBeTrueWhenInput1AndIsFibonacciReturnTrue()
+        {
+            int value = 1;
+            bool expect = true;
+            bool result = myController.isFibonacci(value);
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void itShouldBeTrueWhenInput2AndIsFibonacciReturnTrue()
+        {
+            int value = 2;
+            bool expect = true;
+            bool result = myController.isFibonacci(value);
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void itShouldBeTrueWhenInput3AndIsFibonacciReturnTrue()
+        {
+            int value = 3;
+            bool expect = true;
+            bool result = myController.isFibonacci(value);
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void itShouldBeTrueWhenInput4AndIsFibonacciReturnFalse()
+        {
+            int value = 4;
+            bool expect = false;
+            bool result = myController.isFibonacci(value);
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void itShouldBeTrueWhenInput5AndIsFibonacciReturnTrue()
+        {
+            int value = 5;
+            bool expect = true;
+            bool result = myController.isFibonacci(value);
+            Assert.Equal(expect, result);
+        }
+    }
+}
